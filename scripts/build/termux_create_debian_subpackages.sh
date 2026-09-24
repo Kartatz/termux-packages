4f5b2088c8ae335d91a02a23980a6226b5e5521b
Kartatz <105828205+Kartatz@users.noreply.github.com>
2026-09-24 09:28:29 +0000

    change,scripts: name output .debs by package name only

--- scripts/build/termux_create_debian_subpackages.sh
+++ scripts/build/termux_create_debian_subpackages.sh
@@ -150,7 +150,7 @@ termux_create_debian_subpackages() {
 			-cJf "$SUB_PKG_PACKAGE_DIR/control.tar.xz" -H gnu .
 
 		# Create the actual .deb file:
-		TERMUX_SUBPKG_DEBFILE=$TERMUX_OUTPUT_DIR/${SUB_PKG_NAME}${DEBUG}_${TERMUX_PKG_FULLVERSION}_${SUB_PKG_ARCH}.deb
+		TERMUX_SUBPKG_DEBFILE=$TERMUX_OUTPUT_DIR/${SUB_PKG_NAME}${DEBUG}.deb
 		[[ -f "$TERMUX_COMMON_CACHEDIR/debian-binary" ]] || echo "2.0" > "$TERMUX_COMMON_CACHEDIR/debian-binary"
 		${AR-ar} cr "$TERMUX_SUBPKG_DEBFILE" \
 				   "$TERMUX_COMMON_CACHEDIR/debian-binary" \
