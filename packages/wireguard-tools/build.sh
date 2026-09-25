c197bd7d640538dceff305c3f83f4f5e5cf5acf6
Kartatz <105828205+Kartatz@users.noreply.github.com>
2026-09-25 14:18:16 +0000

    fix,package(wireguard-tools): pin GitHub mirror instead of unstable cgit snapshot

--- packages/wireguard-tools/build.sh
+++ packages/wireguard-tools/build.sh
@@ -4,8 +4,21 @@ TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=1.0.20210914
 TERMUX_PKG_REVISION=3
-TERMUX_PKG_SRCURL=https://git.zx2c4.com/wireguard-tools/snapshot/wireguard-tools-$TERMUX_PKG_VERSION.tar.xz
-TERMUX_PKG_SHA256=97ff31489217bb265b7ae850d3d0f335ab07d2652ba1feec88b734bc96bd05ac
+_COMMIT=3ba6527130c502144e7388b900138bca6260f4e8
+TERMUX_PKG_SRCURL=git+https://github.com/wireguard/wireguard-tools
+TERMUX_PKG_SHA256=683f1f3e1022237e141379ddaded1af4d7e1475cd16fa175615ae3962c7d0a4e
+TERMUX_PKG_AUTO_UPDATE=false
+TERMUX_PKG_GIT_BRANCH=v1.0.20210914
+
+termux_step_post_get_source() {
+	git fetch --unshallow
+	git checkout "$_COMMIT"
+
+	local s=$(find . -type f ! -path '*/.git/*' -print0 | xargs -0 sha256sum | LC_ALL=C sort | sha256sum)
+	if [[ "${s}" != "${TERMUX_PKG_SHA256}  "* ]]; then
+		termux_error_exit "Checksum mismatch for source files."
+	fi
+}
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_EXTRA_MAKE_ARGS=" -C src WITH_BASHCOMPLETION=yes WITH_WGQUICK=no WITH_SYSTEMDUNITS=no"
 
