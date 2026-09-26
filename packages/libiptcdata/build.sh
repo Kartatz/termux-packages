96272b8ba1ae72087d941dfe85713ef511cb347e
Kartatz <105828205+Kartatz@users.noreply.github.com>
2026-09-26 07:51:47 +0000

    fix,package(libiptcdata): build only the library
    The test programs link against the installed library, which is not
    available when building it for the first time.

--- packages/libiptcdata/build.sh
+++ packages/libiptcdata/build.sh
@@ -5,3 +5,9 @@ TERMUX_PKG_MAINTAINER="Gouranga Das Samrat <gouranga.das.khulna@gmail.com>"
 TERMUX_PKG_VERSION="1.0.5"
 TERMUX_PKG_SRCURL=https://github.com/ianw/libiptcdata/releases/download/release_${TERMUX_PKG_VERSION//./_}/libiptcdata-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=c094d0df4595520f194f6f47b13c7652b7ecd67284ac27ab5f219bc3985ea29e
+
+termux_step_make() {
+	# The test programs link against the installed library, which is
+	# not available when building it for the first time.
+	make -C libiptcdata -j $TERMUX_PKG_MAKE_PROCESSES
+}
